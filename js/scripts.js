62492c053f2ad5fd73ac3123f50dd67b25d5bcc0
José Salomón Contreras Vielma <Jos Salomn Contreras Vielma>
2018-10-20 15:45:58 -0300

    'starting-message'

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$('.masthead').paroller();
+
+$('.main-message').paroller({
+  factorXs: 0.1,
+  factorSm: 0.1,
+  factorMd: -0.4,
+  factorLg: -0.4,
+  factorXl: -0.4,
+  factor: -0.4,
+  type: 'foreground',
+  direction: 'vertical'
+});
